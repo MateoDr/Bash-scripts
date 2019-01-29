cd6a28670d57877cd2ba827936cf3bf7a26e26df
MateoDr <mateo.draskovic2@gmail.com>
2019-01-29 10:48:22 +0100

    v2

--- dnscheck.sh
+++ dnscheck.sh
@@ -1,38 +1,48 @@
-#!/user/bin/env bash
-echo "What is the domain we're looking up?"
+#!/usr/bin/env bash
+
+dns_check() {
+
+echo -e "\n\nWhat is the domain we're looking up?"
 read domain
 
-echo -en '\n'
+echo -e "\nHere are the DNS records:"
 
-echo "Here are the DNS records:"
-
-echo -en '\n'
-
-echo "Nameservers:"
+echo -e "\nNameservers:"
 dig @1.1.1.1 $domain NS +short
 
-echo -en '\n'
-
-echo "A record:"
+echo -e "\nA record:"
 dig @1.1.1.1 $domain A +short
 
-echo -en '\n'
-
-echo "MX records:"
+echo -e "\nMX records:"
 dig @1.1.1.1 $domain MX +short
 
-echo -en '\n'
-
-echo "TXT record(s):"
+echo -e "\nTXT record(s):"
 dig @1.1.1.1 $domain TXT +short
 
 dkim="dig @1.1.1.1 _domainkey.$domain"
 
-echo -en '\n'
-
 if [ -z "$dkim" ]
 then
-	echo "This domain does not have a DKIM record"
+	echo -e "\nThis domain does not have a DKIM record"
 else
-	echo "This domain does have a DKIM record"
+	echo -e "\nThis domain does have a DKIM record"
 fi
+}
+
+dns_check
+
+while true; do
+	read -n 1 -p "Would you like to check another domain? (y/n) " checkem
+
+	case $checkem in
+		y) 
+			dns_check
+		;;
+		n) 
+			exit 0
+		;;
+		*) 
+			echo -e "\n\nThat's not a valid character, please type y or n"
+		;;
+	esac
+done
